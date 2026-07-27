5b329df7256bbd0bd7b1bcfcb5750fea4e51316b
fizra00 <fizrafathima786@gmail.com>
2026-07-27 08:58:07 +0530

    Added User Enrollment and Review modules

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+  
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+    public User getUserById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
+
